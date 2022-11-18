9532d48942aafcd21de6700cc689eb3a6e4d2248
SunnyVachhetA <sunnyvachheta89@gmail.com>
2022-11-18 17:46:42 +0530

    Initial commit

---
+++ app/src/main/java/com/skytale/activity/SkyTaleSplashActivity.java
@@ -0,0 +1,28 @@
+package com.skytale.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.skytale.MainActivity;
+import com.skytale.R;
+
+public class SkyTaleSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sky_tale_splash);
+
+        Intent intent = new Intent(SkyTaleSplashActivity.this, MainActivity.class);
+        new Handler(Looper.getMainLooper()).postDelayed
+                (
+                    () -> {
+                        startActivity(intent);
+                        finish();
+                    },
+                    4000
+                );
+    }
+}
